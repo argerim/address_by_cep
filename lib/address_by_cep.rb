5ae67c07eb0af0068f309c4c213d180bfc906a17
Rogerio Medeiros <argerim@gmail.com>
2013-03-19 00:12:17 -0300

    fix generator.

--- lib/address_by_cep.rb
+++ lib/address_by_cep.rb
@@ -25,7 +25,7 @@ module AddressByCep
   end
 
   module Rails
-    require "generators/address_by_cep/engine"
+    require "address_by_cep/engine"
   end
 
 end
---
+++ lib/address_by_cep/address_by_cep/engine.rb
@@ -0,0 +1,6 @@
+module AddressByCep
+	module Rails
+	  class Engine < ::Rails::Engine
+	  end
+	end
+end
