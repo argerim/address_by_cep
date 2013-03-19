35e8c65e0f5cad1452488719a9bd827a01e44494
Rogerio Medeiros <argerim@gmail.com>
2013-03-19 00:45:16 -0300

    fix folders.

--- lib/address_by_cep/generators/version.rb
+++
@@ -1,3 +0,0 @@
-module AddressByCep
-  VERSION = "0.0.5"
-end
--- lib/address_by_cep/templates/address_model.rb
+++
@@ -1,5 +0,0 @@
-class Address < ActiveRecord::Base
-  attr_accessible :zip_code, :city, :complement, :neighborhood, :number, :state, :street
-  belongs_to :addressable, :polymorphic => true
-  validates_presence_of :zip_code, :city, :neighborhood, :state, :street
-end
